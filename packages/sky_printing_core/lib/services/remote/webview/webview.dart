9b40ba16580b9408fbc57b22e7dbe155f9cb8239
runner95up <bokirsianpar95@gmail.com>
2023-11-18 06:58:46 +0700

    feat:add midtrans payment gateway

---
+++ packages/sky_printing_core/lib/services/remote/webview/webview.dart
@@ -0,0 +1,74 @@
+import 'dart:typed_data';
+
+import 'package:sky_printing_core/sky_printing_core.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewClient with MainBoxMixin {
+  String? _auth;
+  bool _isUnitTest = false;
+  late WebViewController _controller;
+
+  WebViewController get controller => _controller;
+
+  WebViewClient({bool isUnitTest = false}) {
+    _isUnitTest = isUnitTest;
+
+    try {
+      _auth = getData(MainBoxKeys.token);
+    } catch (_) {}
+
+    _controller = createBase();
+
+    if (!_isUnitTest) _controller;
+  }
+
+  WebViewController get webViewController {
+    if (_isUnitTest) {
+      return _controller;
+    } else {
+      try {
+        _auth = getData(MainBoxKeys.token);
+      } catch (_) {}
+
+      final controller = createBase();
+
+      if (!_isUnitTest) controller;
+
+      return controller;
+    }
+  }
+
+  WebViewController createBase() => WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          log.i(progress);
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    );
+
+  Future<WebViewController> loadUrl(
+    String url, {
+    LoadRequestMethod method = LoadRequestMethod.get,
+    Map<String, String> headers = const <String, String>{},
+    Uint8List? body,
+  }) async {
+    _controller.loadRequest(
+      Uri.parse(url),
+      headers: headers,
+      body: body,
+      method: method,
+    );
+    return _controller;
+  }
+}
