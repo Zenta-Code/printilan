e74d1b9b063f09d122168759db953d8b2b91e521
runner95up <bokirsianpar95@gmail.com>
2023-11-20 05:29:58 +0700

    feat:add api order callback

--- apps/api/src/controller/order.ts
+++ apps/api/src/controller/order.ts
@@ -63,6 +63,7 @@ export const OrderController = ({ route }: { route: Router }) => {
     try {
       const body = req.body;
       console.log("body: ", body);
+      res.status(200).json(body);
     } catch (error) {
       console.log("error: ", error);
     }
